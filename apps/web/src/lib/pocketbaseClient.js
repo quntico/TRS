2629ded97572a5240d46aaa0f61f09585af57d16
Freddy Arevalo <delavega3540@gmail.com>
2026-07-03 16:09:23 -0600

    Configure pocketbaseClient to use local server port 8090 on localhost

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -1,9 +1,12 @@
 import Pocketbase from 'pocketbase';
 
-const POCKETBASE_API_URL = '/hcgi/platform';
+const POCKETBASE_API_URL = typeof window !== 'undefined' && (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+  ? 'http://localhost:8090'
+  : '/hcgi/platform';
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 
 export default pocketbaseClient;
 
 export { pocketbaseClient };
+
